a083a7fee7a75aa1a7be9bd8a95b8b1954a450d7
Simon Martyr <simon.martyr@finaps.nl>
2024-09-18 17:32:31 +0200

    ci: 🎡 weekly test run including more integrtion tests (#11)

--- article_details_test.go
+++ article_details_test.go
@@ -24,6 +24,24 @@ func TestGetArticleDetails(t *testing.T) {
 	}
 }
 
+func Test_Integration_GetArticleDetails(t *testing.T) {
+	articleId := "s1005863"
+	c := intClient(t)
+	res, err := c.GetArticleDetails(articleId)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.Name != "Koffiecups recyclezak" {
+		t.Error("Invalid article name")
+	}
+	if res.Description.Main == "" {
+		t.Error("Invalid description")
+	}
+	if len(res.Images) != 1 {
+		t.Error("Invalid images")
+	}
+}
+
 func TestGetArticleDetails_Error_MissingId(t *testing.T) {
 	articleId := "  "
 	c, s := testClientFile(http.StatusOK, "test/article_details_data.json")
